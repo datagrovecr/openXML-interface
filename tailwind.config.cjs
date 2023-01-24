c5f7b31e777e1c57f2eb818bc02da68d1595f877
Andres Villalobos <andres.villalobos@datagrove.com>
2023-01-24 13:10:52 -0600

    colors config txt +  layout changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors:{
-      'green':{
-        500: '#10B981',
-        600: '#059669',
-        700: '#047857',
-        800: '#065F46'
-      },
-      'grey':'#18181B',
-      'white':'#ffffff'
-
-    },
     extend: {},
   },
   plugins: [],
