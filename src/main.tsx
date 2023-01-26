6e508cbe489f504bee03d56bb3852dbab4926b89
Andres Villalobos <andres.villalobos@datagrove.com>
2023-01-25 18:07:45 -0600

    removed convert button

--- src/main.tsx
+++ src/main.tsx
@@ -18,18 +18,20 @@ const Home = () => {
   return <>
     <Center>
 
-      <div><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount(count() + 1)}>+</button>  Counter: {count}</div>
+      <div><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount(count() + 1)}>+</button>  Counter: {count} Welcome to the markdown converter</div>
     </Center>
   </>
 }
 const About = () => {
   return <Center>
-    <div class='m-2 p-2 rounded-md bg-white text-black' >Typescript+Solid+Vite</div>
+    <div class='m-2 p-2 rounded-md bg-white text-black' >Descriptions, what is markdown, why use it, is the converter secure, etc.</div>
   </Center>
 }
 const EditorPage = () => {
-  return  <div class='m-2 p-2 rounded-md bg-white text-black h-screen' ><Editor/></div>
+  return  <div class='m-2 p-2 rounded-md bg-white dark:bg-black text-black dark:text-white max-h-screen sm:max-h-screen' ><Editor/></div>
 }
+
+const SideMenu = () => {return ('hello')}
 function App() {
   return <>
     <nav>
