1b7f86a685fa054cba2386841531b53ae808911e
Andres Villalobos <andres.villalobos@datagrove.com>
2023-02-13 13:15:58 -0600

    integrating content from previous menu

--- src/main.tsx
+++ src/main.tsx
@@ -111,70 +111,65 @@ function SideBar(){
       </div>
 </div> 
 }function SideBar2(){
-  //const [mode,setMode] = createSignal(false)
+  const [mode,setMode] = createSignal(false)
   return <div class="w-full h-full absolute top-0 left-0">
       <div class="flex flex-no-wrap">
           {/* <!-- Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] --> */}
           {/* <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ ---> */}
-          <div style="min-height: 716px" class="w-64 absolute sm:relative bg-gray-800 shadow md:h-full flex-col justify-between hidden sm:flex">
+          <div style="min-height: 716px" class="w-64 absolute sm:relative bg-slate-400 dark:bg-gray-800 shadow md:h-full flex-col justify-between hidden sm:flex">
               <div class="px-8">
                   <div class="h-16 w-full flex items-center">
                     <h1 id='menuTitle' class='text-white font-bold'>File converter</h1>
                   </div>
                   <ul class="mt-12">
-                      <li class="flex w-full justify-between text-gray-300 cursor-pointer items-center mb-6">
+                      <li class="flex w-full justify-between text-black cursor-pointer items-center mb-6">
                           <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
                               <span class="text-sm ml-2">Download File</span>
                           </a>
                           {/* <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs"></div> */}
                       </li>
-                      <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
+                      <li class="flex w-full justify-between text-black dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer items-center mb-6">
                           <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
                               <span class="text-sm ml-2">Upload File</span>
                           </a>
                           {/* <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">8</div> */}
                       </li>
-                      <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
-                          <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                      <li class="flex w-full justify-between text-black dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer items-center mb-6">
+                          <a href="javascript:void(0)" class="flex items-center dark:focus:outline-none dark:focus:ring-2 dark:focus:ring-white">
                               <span class="text-sm ml-2">Performance</span>
                           </a>
                       </li>
-                      <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
-                          <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white" >
+                      <li class="flex w-full justify-between text-black dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer items-center mb-6">
+                          <a href="javascript:void(0)" class="flex items-center dark:focus:outline-none dark:focus:ring-2 dark:focus:ring-white" >
                               <span class="text-sm ml-2">Deliverables</span>
                           </a>
                       </li>
-                      <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
-                          <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                      <li class="flex w-full justify-between text-black dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer items-center mb-6">
+                          <a href="javascript:void(0)" class="flex items-center dark:focus:outline-none dark:focus:ring-2 dark:focus:ring-white">
                               <span class="text-sm ml-2">Invoices</span>
                           </a>
                       </li>
-                      <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
-                          <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                      <li class="flex w-full justify-between text-black dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer items-center mb-6">
+                          <a href="javascript:void(0)" class="flex items-center focus:outline-none dark:focus:ring-2 dark:focus:ring-white">
                               <span class="text-sm ml-2">Inventory</span>
                           </a>
                       </li>
-                      <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center">
-                          <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
-                              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                  <path stroke="none" d="M0 0h24v24H0z" />
-                                  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                  <circle cx="12" cy="12" r="3" />
-                              </svg>
+                      <li class="flex w-full justify-between text-black dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer items-center">
+                          <a href="#" class="flex items-center focus:outline-none dark:focus:ring-2 dark:focus:ring-white">
                               <span class="text-sm ml-2">Settings</span>
                           </a>
                       </li>
                   </ul>
                   <div class="flex justify-center mt-48 mb-4 w-full">
                       <div class="relative">
-                          <div class="text-gray-300 absolute ml-4 inset-0 m-auto w-4 h-4">
+                          <div class="text-black dark:text-gray-300 absolute ml-4 inset-0 m-auto w-4 h-4">
                               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                   <path stroke="none" d="M0 0h24v24H0z"></path>
                                   <circle cx="10" cy="10" r="7"></circle>
                                   <line x1="21" y1="21" x2="15" y2="15"></line>
                               </svg>
                           </div>
-                          <input class="bg-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-100 rounded w-full text-sm text-gray-300 placeholder-gray-400 bg-gray-100 pl-10 py-2" type="text" placeholder="Search" />
+                          <input class="dark:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-100 rounded w-full text-sm text-black placeholder-gray-400 bg-gray-100 pl-10 py-2" type="text" placeholder="Search" />
                       </div>
                   </div>
               </div>
@@ -216,6 +211,9 @@ function SideBar(){
                                   <line x1="10" y1="12" x2="14" y2="12"></line>
                               </svg>
                           </button>
+                      </li>
+                      <li>
+
                       </li>
                   </ul>
               </div>
@@ -379,6 +377,33 @@ function SideBar(){
                                   <line x1="10" y1="12" x2="14" y2="12"></line>
                               </svg>
                           </button>
+                      </li>
+                      <li>
+                      <button onclick={
+        function(){
+        setMode(!mode())
+        const userTheme = localStorage.getItem("theme");
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        if(userTheme==="dark"|| (!userTheme && systemTheme)){
+          document.documentElement.classList.add("dark");
+          themeToggle();
+          return;
+      }else{
+        themeToggle();
+      }
+      function themeToggle(){
+        if(document.documentElement.classList.contains("dark")){
+          document.documentElement.classList.remove("dark");
+          localStorage.setItem("theme", "light");
+          return;
+      }else{
+          document.documentElement.classList.add("dark");
+          localStorage.setItem("theme","dark");
+      }
+      }
+      }}>
+        <Icon path={mode()?sun:moon} id="light" style="width: 24px; color:black dark:color: white hover:bg-slate-500" />
+        </  button>
                       </li>
                   </ul>
               </div>
